d46e5e4e82ba0ee73c3f2599a31247f6019f6719
Tim <timconwaydev@outlook.com>
2024-11-07 13:25:41 -0500

    Protected Main Routes to require signing in

--- client/MainRouter.jsx
+++ client/MainRouter.jsx
@@ -1,38 +1,51 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './core/Home'
-import AddConcerts from './core/AddConcerts.jsx'
-import Users from './user/Users.jsx'
-import Signup from './user/Signup.jsx'
-import Signin from './lib/Signin.jsx'
-import Profile from './user/Profile.jsx'
-import PrivateRoute from './lib/PrivateRoute.jsx'
-import EditProfile from './user/EditProfile.jsx'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./core/Home";
+import AddConcerts from "./core/AddConcerts.jsx";
+import Users from "./user/Users.jsx";
+import Signup from "./user/Signup.jsx";
+import Signin from "./lib/Signin.jsx";
+import Profile from "./user/Profile.jsx";
+import PrivateRoute from "./lib/PrivateRoute.jsx";
+import EditProfile from "./user/EditProfile.jsx";
 
-import Menu from './core/Menu'
+import Menu from "./core/Menu";
 function MainRouter() {
-    return (
-        <div>
-            <Menu />
+  return (
+    <div>
+      <Menu />
 
-
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/addconcerts" element={<AddConcerts />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/signin" element={<Signin />} />
-                <Route
-                    path="/user/edit/:userId"
-                    element={
-                        <PrivateRoute>
-                            <EditProfile />
-                        </PrivateRoute>
-                    }
-                />
-                <Route path="/user/:userId" element={<Profile />} />
-            </Routes>
-        </div>
-    );
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/addconcerts"
+          element={
+            <PrivateRoute>
+              <AddConcerts />
+            </PrivateRoute>
+          }
+        />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route
+          path="/user/edit/:userId"
+          element={
+            <PrivateRoute>
+              <EditProfile />
+            </PrivateRoute>
+          }
+        />
+        <Route path="/user/:userId" element={<Profile />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default MainRouter
+export default MainRouter;
